4ae495a469a85840c4c034d6a25f6b063e83cb64
KudzoNelsam <kudzo2004@gmail.com>
2026-01-10 16:30:20 +0000

    Modification  Majeure de l'application

--- lib/data.ts
+++
@@ -1,3 +0,0 @@
-export type { GP, Trip, Message, Conversation } from "./models"
-
-export { MOCK_GPS, MOCK_CONVERSATIONS, MOCK_MESSAGES, POPULAR_DESTINATIONS, CITIES_DEPART, CITIES_DESTINATION } from "./mocks/data"
